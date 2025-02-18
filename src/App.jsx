6b6783184ae3467d1bc60f70fefa626ac4043035
Andres <andresperlo5@gmail.com>
2025-02-17 21:34:03 -0300

    inicio de proyecto web9

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+
+  //<> fragments
+  //componentes
+  //rutas
+  return (
+    <>
+      <h1>Hola</h1>
+      <h2>Hola</h2>
+
+      <p>Soy un parrafo</p>
+    </>
+  )
+}
+
+export default App
